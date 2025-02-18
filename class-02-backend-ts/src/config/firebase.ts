cc667b607e80139842530cfe639b9d17c5124914
0SU2 <or_zv18v@outlook.com>
2025-02-18 08:28:52 -0600

    [CHANGED] Llaves de firebase

--- class-02-backend-ts/src/config/firebase.ts
+++ class-02-backend-ts/src/config/firebase.ts
@@ -2,7 +2,11 @@ import admin from 'firebase-admin';
 import serviceAccount  from './serviceAccountKey.json';
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
+    credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        privateKey: process.env.FIREBASE_PRIVATE?.replace(/\\n/g, '\n'),
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    }),
 });
 
 const db = admin.firestore();
